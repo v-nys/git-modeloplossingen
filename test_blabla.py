e4d832a74a1fe7f9aa646d938901b511f61a98c8
Vincent Nys <vincentnys@gmail.com>
2020-03-21 16:42:18 +0100

    Om te testen met errors ipv failures.

--- test_blabla.py
+++ test_blabla.py
@@ -6,4 +6,5 @@ def _model_solution(number):
 
 @pytest.mark.parametrize("input1",[0,255,128,-1,256])
 def test_is_byte(input1):
-	assert python_printen.is_byte(input1) == _model_solution(input1),f"verschil met modeloplossing voor invoer {input1}"
+    raise ValueError('test error')
+    assert python_printen.is_byte(input1) == _model_solution(input1),f"verschil met modeloplossing voor invoer {input1}"
